bc97a8795d0783af6774089d6442e1a6dc2f3fa7
root <root@bguney-intern-app-server.c.devops-interns.internal>
2025-05-21 11:55:58 +0000

    Placeholders added.

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,19 +20,19 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'db_name_holder' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'db_user_holder' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'db_password_holder' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db_host_holder' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'db_charset_holder' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         'auth_key_holder' );
+define( 'SECURE_AUTH_KEY',  'secure_auth_key_holder' );
+define( 'LOGGED_IN_KEY',    'logged_in_key_holder' );
+define( 'NONCE_KEY',        'nonce_key_holder' );
+define( 'AUTH_SALT',        'auth_salt_holder' );
+define( 'SECURE_AUTH_SALT', 'secure_auth_salt_holder' );
+define( 'LOGGED_IN_SALT',   'logged_in_salt_holder' );
+define( 'NONCE_SALT',       'nonce_salt_holder' );
 
 /**#@-*/
 
